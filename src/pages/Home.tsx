ab715b5daa42c2237264bc155455b156d7f3884f
whiskacz <pawelblasiak1985@gmail.com>
2023-11-08 22:19:39 +0100

    background done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,23 +3,22 @@ import '../css/main.css'
 import { GiFilmStrip } from "react-icons/gi";
 
 export const Home = () => {
+
   return (
     <>
       <div className='backgroundWrapper'>
-        <li><GiFilmStrip style={{ 
-          width: `${Math.floor(Math.random()* 5)+ 25}rem`
-          }}/></li>
-        <li><GiFilmStrip /></li>
-        <li><GiFilmStrip /></li>
-        <li><GiFilmStrip /></li>
-        <li><GiFilmStrip /></li>
-        <li><GiFilmStrip /></li>
-        <li><GiFilmStrip /></li>
-        <li><GiFilmStrip /></li>
-        <li><GiFilmStrip /></li>
-        <li><GiFilmStrip /></li>
-
+        <li />
+        <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
+        <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
+        <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
+        <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
+        <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
+        <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
+        <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
+        <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
+        <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
       </div>
+      
     </>
   )
 }
