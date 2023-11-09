184068b0cd653e24bc7e00d06d4a8021c6a8981c
whiskacz <pawelblasiak1985@gmail.com>
2023-11-09 22:45:35 +0100

    loginContainer in progress

---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const LoginPage = () => {
+  return (
+
+    <main className='loginPageWrapper'>
+        <div className='choiceContainer'>
+        
+        </div>
+
+        <div className='loginPageBox'>
+        </div>    
+    </main>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import '../css/main.css'
 import { GiFilmStrip } from "react-icons/gi";
+import { LoginPage } from '../components/LoginPage';
 
 export const Home = () => {
 
+  const [showLogin, setShowLogin] = useState<boolean>(false) 
+  const handleShowLogin = () => {
+    setTimeout(()=>{
+      setShowLogin(true);
+    }, 400)
+  }
+
+
   return (
     <>
-      <div className='backgroundWrapper'>
+      <div className='backgroundWrapper' onClick={handleShowLogin}>
         <li />
         <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
         <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
@@ -18,7 +27,13 @@ export const Home = () => {
         <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
         <li style={{left: `${Math.floor(Math.random() * (90))}%`}}><GiFilmStrip style={{width: `${Math.floor(Math.random()* 15)+ 2}rem`, height: `${Math.floor(Math.random()* 15)+ 2}rem`, left: `${Math.floor(Math.random() * (100))}%`, animationDelay: `${Math.floor(Math.random() * (15))}s`, animationDuration: `${Math.floor(Math.random() * (200)+15)}s`, opacity: `${Math.random()*(0.4-0.05)+0.1}`}}/></li>
       </div>
-      
+      {showLogin ? 
+      <LoginPage /> : 
+      <div className='mainTitle'>
+        <p>Welcome,</p>
+        <p>click to start</p>
+      </div>
+      }
     </>
   )
 }
